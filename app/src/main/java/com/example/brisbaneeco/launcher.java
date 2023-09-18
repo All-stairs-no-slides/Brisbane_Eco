c80cd01712b4e22e7d39f72b6ce84a04ee202e24
Alistair <38995281+All-stairs-no-slides@users.noreply.github.com>
2023-09-18 17:38:07 +1000

    the bloody project

---
+++ app/src/main/java/com/example/brisbaneeco/launcher.java
@@ -0,0 +1,14 @@
+package com.example.brisbaneeco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
