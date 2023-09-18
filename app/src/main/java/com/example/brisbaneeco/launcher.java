00e873c11b2d0c00248b5eee5a0c33dcb371553a
Liam <liam.t4@hotmail.com>
2023-09-18 19:28:21 +1000

    Created Login and Signup pages. Login is empty, Signup almost complete

--- app/src/main/java/com/example/brisbaneeco/launcher.java
+++ app/src/main/java/com/example/brisbaneeco/launcher.java
@@ -2,13 +2,39 @@ package com.example.brisbaneeco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class launcher extends AppCompatActivity {
+
+    private TextView TitleTextView;
+    private Button SignUp;
+    private Button LogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        setViewIds();
+
+        SignUp.setOnClickListener(view -> {
+            Intent intent = new Intent(launcher.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
+        LogIn.setOnClickListener(view -> {
+            Intent intent = new Intent(launcher.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
+    private void setViewIds()
+    {
+        TitleTextView = findViewById(R.id.title);
+        SignUp = findViewById(R.id.btnSignup);
+        LogIn = findViewById(R.id.btnLogin);
+    }
+
 }
