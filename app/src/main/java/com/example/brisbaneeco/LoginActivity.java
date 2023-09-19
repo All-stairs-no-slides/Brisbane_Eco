6b8a61114e65c83b43c5509cd5dd987df19c3d96
Alistair <38995281+All-stairs-no-slides@users.noreply.github.com>
2023-09-19 13:38:11 +1000

    Login activity done

--- app/src/main/java/com/example/brisbaneeco/LoginActivity.java
+++ app/src/main/java/com/example/brisbaneeco/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.brisbaneeco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button LoggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setviewIds();
+
+        LoggedIn.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setviewIds()
+    {
+        LoggedIn = findViewById(R.id.LoginButton);
     }
 }
