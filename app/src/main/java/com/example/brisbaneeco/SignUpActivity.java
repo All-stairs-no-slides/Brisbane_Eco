e7ab02234a7acf4522dbfb8ce831d43d37cd5146
liamt4 <liam.t4@hotmail.com>
2023-09-19 13:08:28 +1000

    finished SignUpActivity and added a blank MainMenuActivity

--- app/src/main/java/com/example/brisbaneeco/SignUpActivity.java
+++ app/src/main/java/com/example/brisbaneeco/SignUpActivity.java
@@ -2,13 +2,29 @@ package com.example.brisbaneeco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button SignedUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        setviewIds();
+
+        SignedUp.setOnClickListener(view -> {
+            Intent intent = new Intent(SignUpActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setviewIds()
+    {
+        SignedUp = findViewById(R.id.SignedUpButton);
     }
 }
