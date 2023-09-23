bdc041be6ef5bb8f0546b81935f03e49ba07622b
LANA <104051421+pleasehelpmeLANA@users.noreply.github.com>
2023-09-23 20:25:24 +1000

    FInished medal page design
    FInished medal page design

---
+++ app/src/main/java/com/example/brisbaneeco/MedalsPage.java
@@ -0,0 +1,14 @@
+package com.example.brisbaneeco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedalsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medals_page);
+    }
+}
