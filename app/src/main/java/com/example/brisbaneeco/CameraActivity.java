21264e8a43fbdd178e509013edfbef9831596394
Alistair <38995281+All-stairs-no-slides@users.noreply.github.com>
2023-09-24 19:13:51 +1000

    fixed some stuff

--- app/src/main/java/com/example/brisbaneeco/CameraActivity.java
+++ app/src/main/java/com/example/brisbaneeco/CameraActivity.java
@@ -2,13 +2,28 @@ package com.example.brisbaneeco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CameraActivity extends AppCompatActivity {
 
+
+    private Button AddP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        setviewIds();
+
+        AddP.setOnClickListener(view -> {
+            Intent intent = new Intent(CameraActivity.this, AddPoints.class);
+            startActivity(intent);
+        });
+    }
+    private void setviewIds()
+    {
+        AddP = findViewById(R.id.btnLogin);
     }
 }
