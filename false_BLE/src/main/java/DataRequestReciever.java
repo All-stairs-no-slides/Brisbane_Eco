76a2d923e77a74907bd9a9bea31423febda38292
Alistair <38995281+All-stairs-no-slides@users.noreply.github.com>
2023-10-17 23:52:36 +1000

    false_BLE

---
+++ false_BLE/src/main/java/DataRequestReciever.java
@@ -0,0 +1,23 @@
+import static spark.Spark.*;
+
+public class DataRequestReciever {
+
+    public static void main(String[] args) {
+        // Define the port for your server
+        int port = 4567;
+        port(port);
+        // Define an endpoint to receive data via POST request
+        post("/receiveData", "application/json", (request, response) -> {
+        // Handle the received data
+            String data = request.body();
+            System.out.println("Received request: " + data);
+            // You can process the data further or save it to a database
+            // processing and sending response back
+            if (data.contains("Temperature")){
+                return "Requires Accelerometer Data";
+            }else {
+                return "Data received successfully!";
+            }
+        });
+    }
+}
