65a91daf86668c37a8808afb44c8fe57f1b0acc9
LANA <104051421+pleasehelpmeLANA@users.noreply.github.com>
2023-10-26 19:18:46 +1000

    Working on QR code scanner
    Working on QR code scanner

--- app/src/main/java/com/example/brisbaneeco/CameraActivity.java
+++ app/src/main/java/com/example/brisbaneeco/CameraActivity.java
@@ -4,26 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnScanBarcode;
 
-    private Button AddP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
-        setviewIds();
-
-        AddP.setOnClickListener(view -> {
-            Intent intent = new Intent(CameraActivity.this, AddPoints.class);
-            startActivity(intent);
-        });
+        initViews();
     }
-    private void setviewIds()
-    {
-        AddP = findViewById(R.id.btnLogin);
+
+    private void initViews() {
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnScanBarcode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(CameraActivity.this, ScannerActivity.class));
     }
 }
